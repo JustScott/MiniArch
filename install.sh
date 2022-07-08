064fce9444217c6539518c3e29e5d04c7592c180
Scott <justscottmail@protonmail.com>
2022-07-08 02:54:30 +0000

    Added option to enable/disable disk encryption

--- install.sh
+++ install.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
+echo -n 'Use Encrypted System?[y/n](default:n): '
+read encrypt_system
+
+----------------  Parition Configuration ----------------
+
+# Boot Parition
+mkfs.ext4 /dev/sda1
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+# Swap Partition
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# Encrypted Storage Partition
+if [ encrypt_system == 'y' ] || [ encrypt_system == 'Y' ] || [ encrypt_system == 'yes' ]
+then
+  cryptsetup luksFormat -s 512 -h sha512 /dev/sda3
+  cryptsetup open /dev/sda3 cryptdisk
+  mkfs.ext4 /dev/mapper/cryptdisk
+  mount /dev/mapper/cryptdisk /mnt
+else
+  mkfs.ext4 /dev/sda3
+  mount /dev/sda3 /mnt
+if
+
+----------------  Network Configuration ----------------
+clear
+
+echo -n 'Do you need to setup a wirless connection?[y/n](default:y): '
+read wireless
+
+if [ $wireless == 'y' ] || [ $wireless == '' ]
+then
+  echo ' -- Use These Commands -- '
+  echo 'device list'
+  echo 'station wlan0 scan'
+  echo 'station wlan0 get-networks'
+  echo 'station wlan0 connect <ssid>'
+  echo 'exit'
+  iwctl
+fi
+
+----------------  System Settings & Packages ----------------
+clear
+
 # Install the default pacman application
 pacman -S --noconfirm gnome-control-center gnome-backgrounds gnome-terminal gnome-settings-daemon gnome-calculator gdm file-roller grub xorg networkmanager sudo htop git base-devel man-db man-pages
 
+# Base filesystem packages
 pacstrap /mnt base linux linux-firmware vim
+
+# Tell the system where the partitions are when starting
+genfstab -U /mnt >> /mnt/etc/fstab
 
 # Enter the filesystem as root
 arch-chroot /mnt
@@ -14,23 +64,26 @@ echo LANG='en_US.UTF-8' > /etc/locale.conf
 export LANG=en_US.UTF-8
 locale-gen
 
-----------------  System User Configuration ----------------
 
-echo 'Enter System Name:'
+----------------  System User Configuration ----------------
+clear
+
+echo -n 'Enter System Name: '
 read system_name
 echo System Name Set as $system_name
 
 # Set the root password
-echo 'Set root password:'
+echo 'Set root password'
 passwd
 
 echo archy > /etc/hostname
-echo 127.0.0.1   localhost$'\n'::1         localhost$'\n'127.0.1.1   archy >> /etc/hosts
+echo -e '127.0.0.1   localhost\n::1         localhost\n127.0.1.1   archy' >> /etc/hosts
 
 
 ----------------  User Configuration ----------------
+clear
 
-echo 'Enter Username:'
+echo -n 'Enter Username: '
 read $username
 echo Username set as $username
 
@@ -42,11 +95,15 @@ sleep 3
 EDITOR=vim visudo
 
 ----------------  Grub Configuration ----------------
+clear
 
-# Encryption configuration
-echo GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda3:cryptdisk"$'\n'GRUB_ENABLE_CRYPTDISK=y >> /etc/default/grub
+if [ encrypt_system == 'y' ] || [ encrypt_system == 'Y' ] || [ encrypt_system == 'yes' ]
+then
+  # Encryption configuration
+  echo -e 'GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda3:cryptdisk"\nGRUB_ENABLE_CRYPTDISK=y' >> /etc/default/grub
+  echo -e 'MODULES=()\nBINARIES\nFiles=()\nHOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)' > /etc/mkinitcpio.conf
+fi
 
-echo MODULES=()$'\n'BINARIES$'\n'Files=()$'\n''HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)' > /etc/mkinitcpio.conf
 mkiniticpio -p linux
 
 # Actual Grub Install
@@ -54,6 +111,7 @@ grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 ----------------  Final Touches  ----------------
+clear
 
 # Enabling display and network managers
 systemctl enable gdm.service
