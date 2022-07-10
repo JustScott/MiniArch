739e766a8ac7bdb15c0aeb05fbfe2b212857bfb7
Scott <justscottmail@protonmail.com>
2022-07-09 22:36:59 -0500

    Update and rename install.sh to start_install.sh

---
+++ start_install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+clear
+
+echo -n 'Using Encrypted System?[y/n](default:n): '
+read encrypt_system
+
+
+#----------------  Parition Formatting ---------------- 
+  
+if [ $encrypt_system == 'y' ] || [ $encrypt_system == 'Y' ] || [ $encrypt_system == 'yes' ]
+then
+  # Encrypted Filesystem Partition
+  cryptsetup luksFormat -s 512 -h sha512 /dev/sda3
+  cryptsetup open /dev/sda3 cryptdisk
+  mkfs.ext4 /dev/mapper/cryptdisk
+  mount /dev/mapper/cryptdisk /mnt
+else
+  # Unencrypted Filesystem Partition
+  mkfs.ext4 /dev/sda3
+  mount /dev/sda3 /mnt
+fi
+
+# Swap Partition
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# Boot Parition
+mkfs.ext4 /dev/sda1
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+
+#----------------  /mnt Prepping ----------------
+
+# Put the basic linux dependices on your filesystem parition, + vim,git
+pacstrap /mnt base linux linux-firmware
+
+# Tell the system where the partitions are when starting
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Move our final script to /mnt
+mv MiniArch/finish_install.sh /mnt
+
+# Remove the scripts old scripts
+rm -rf MiniArch
+
+# Move into /mnt
+clear
+echo '\n - Type This Command: source finish_install.sh - \n'
+arch-chroot /mnt
+
+# After finish_install.sh is done
+clear
+umount -a
+echo -e '\n - Remove the installation media before starting the system again - \n'
+echo 'Shutting down in 10 seconds...'
+sleep 10
+
+shutdown now
