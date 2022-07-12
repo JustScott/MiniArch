127cac2d151c799ae408bec9bc9a43ec2478d227
Scott <justscottmail@protonmail.com>
2022-07-12 10:41:54 -0500

    Update create_partition_table.py

--- create_partition_table.py
+++ create_partition_table.py
@@ -16,30 +16,30 @@ with open('uefi_state.temp', 'r')as f:
 
 if uefi:
     table = f'''
-    label: gpt
-    device: /dev/{disk_label}
-    unit: sectors
-    first-lba: 2048
-    sector-size: 512
+label: gpt
+device: /dev/{disk_label}
+unit: sectors
+first-lba: 2048
+sector-size: 512
 
-    /dev/{disk_numbering} : start=        2048, size=     1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-    /dev/{disk_numbering} : start=     1050624, size=     4194304, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
-    /dev/{disk_numbering} : start=     5244928, size=  , type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
-    '''
+/dev/{disk_numbering} : start=        2048, size=     1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+/dev/{disk_numbering} : start=     1050624, size=     4194304, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+/dev/{disk_numbering} : start=     5244928, size=  , type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+'''
 
 else:
-    dos = f'''
-    label: dos
-    device: /dev/{disk_label}
-    unit: sectors
-    sector-size: 512
+    table = f'''
+label: dos
+device: /dev/{disk_label}
+unit: sectors
+sector-size: 512
 
-    /dev/{disk_numbering} : start=        2048, size=     1048576, type=83, bootable
-    /dev/{disk_numbering} : start=     1050624, size=     4194304, type=82
-    /dev/{disk_numbering} : start=     5244928, size=     , type=83
-    '''
+/dev/{disk_numbering} : start=        2048, size=     1048576, type=83, bootable
+/dev/{disk_numbering} : start=     1050624, size=     4194304, type=82
+/dev/{disk_numbering} : start=     5244928, size=     , type=83
+'''
 
 
-with open('root/MiniArch/partition_table.txt', 'w')as f:
+with open('partition_table.txt', 'w')as f:
     f.write(table)
     
