3cc7fe92d8701752043331dc24d8d125ec206137
Scott <justscottmail@protonmail.com>
2022-07-12 12:40:32 -0500

    Update finish_install.sh

--- finish_install.sh
+++ finish_install.sh
@@ -67,9 +67,6 @@ usermod -aG wheel,audio,video,storage $username
 #----------------  System Settings & Packages ----------------
 clear
 
-# Install the default pacman application
-pacman -S --noconfirm gnome-control-center gnome-backgrounds gnome-terminal gnome-keyring gnome-logs gnome-settings-daemon gnome-calculator gnome-software gvfs malcontent mutter gdm nautilus xdg-user-dirs-gtk grub xorg networkmanager sudo htop base-devel vim git man-db man-pages
-
 echo -e '\n##Appended to file via install script (MiniArch)\n%wheel ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
 # Set the keyboard orientation
