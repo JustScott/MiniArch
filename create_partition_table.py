05792a858d7df7c47428d4a99b88f1ed18e7ff81
JustScott <development@justscott.me>
2025-02-17 11:26:44 -0600

    Updated copyright notices

--- create_partition_table.py
+++ create_partition_table.py
@@ -1,6 +1,6 @@
 #
 # create_partition_table.py - part of the MiniArch project
-# Copyright (C) 2024, JustScott, development@justscott.me
+# Copyright (C) 2024-2025, JustScott, development@justscott.me
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
--- finish_install.sh
+++ finish_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # finish_install.sh - part of the MiniArch project
-# Copyright (C) 2024, JustScott, development@justscott.me
+# Copyright (C) 2024-2025, JustScott, development@justscott.me
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
